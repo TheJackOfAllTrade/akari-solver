5ef9b3b5c6b5cac07a2a04c26289ba1a62851ef8
TheJackOfAllTrade <hendersonshamus03@gmail.com>
2025-01-30 22:15:05 +0000

    Made start on pre processing for fours
    There is a dumb error there but at least it's started right?

--- source/main.py
+++ source/main.py
@@ -4,11 +4,11 @@ import preprocessing as pp
 class Cell: #Each cell in the puzzle board and its attributes
     def __init__(self, cellType, lit, lightable):
         self.cellType = cellType
-        self.lit = lit
-        self.lightable = lightable
+        self.lit = lit #This is whether a cell is lit by a lightbulb or not
+        self.lightable = lightable #Don't be fooled, "Lightable" in this case means a cell can't have a lightbulb. I'm just dumb and too lazy to change it
 
 def readFile(): #Reads the file and formats each cell into the Cell object, adding them to the puzzleBoard array
-    file = open("akari-solver/puzzles/7x7-2", "r")
+    file = open("akari-solver/puzzles/7x7-6", "r")
     content = file.readlines()
 
     for i in content[7:(len(content)-1)]:
@@ -31,14 +31,15 @@ def readFile(): #Reads the file and formats each cell into the Cell object, addi
 puzzleBoard = [] #The initial puzzleboard, a 2D array of "Cell" Objects
 readFile() #Initialises puzzleBoard
 print("Puzzle Initialised!")
-print(repr(puzzleBoard[1][1].cellType))
 
 proPuzzleBoard = pp.preProcess(puzzleBoard) #Calls function in preprocessing.py
 print("Puzzle preprocessed")
 
-print(proPuzzleBoard[0][2].lightable)    # For the case of
-print(proPuzzleBoard[0][1].lightable)    #    - - -
-print(proPuzzleBoard[1][0].lightable)    #    - 0 -
-print(proPuzzleBoard[1][2].lightable)    #    - - -
-print(proPuzzleBoard[2][1].lightable)    # Testing Purposes (Expected: True False False False False)
+for i in range(len(proPuzzleBoard)):
+    print("##########################")
+    print(proPuzzleBoard[i][5].cellType)
+    print(proPuzzleBoard[i][5].lit)
+
+
+
 
--- source/preprocessing.py
+++ source/preprocessing.py
@@ -4,14 +4,25 @@
 def preProcess(puzzleBoard): #What is called in the main
     processedPuzzleBoard = puzzleBoard #Doesn't overwrite puzzleBoard just in case
     processedPuzzleBoard = lookForZero(processedPuzzleBoard)
+    processedPuzzleBoard = lookForFour(processedPuzzleBoard)
     return processedPuzzleBoard
 
 def lookForZero(processedPuzzleBoard): #Searches the board for any cells containing a zero
     for y in range(len(processedPuzzleBoard)):
         for x in range(len(processedPuzzleBoard[y])):
             if processedPuzzleBoard[y][x].cellType == "0":
-                print("Setting (" + str(x) + "," + str(y) + ")")
+                print("Found 0 at (" + str(x) + "," + str(y) + ")")
                 processedPuzzleBoard = setZero(processedPuzzleBoard, x, y)
+            else:
+                next
+    return processedPuzzleBoard
+
+def lookForFour(processedPuzzleBoard): #Searches the board for any cells containing a four
+    for y in range(len(processedPuzzleBoard)):
+        for x in range(len(processedPuzzleBoard[y])):
+            if processedPuzzleBoard[y][x].cellType == "4":
+                print("Found 4 at (" + str(x) + "," + str(y) + ")")
+                processedPuzzleBoard = setFour(processedPuzzleBoard, x, y)
             else:
                 next
     return processedPuzzleBoard
@@ -22,15 +33,15 @@ def setZero(puzzleBoard, x , y): #Sets surrounding cells as unlightable
         processedPuzzleBoard[y][x+1].lightable = False
         processedPuzzleBoard[y+1][x].lightable = False
         processedPuzzleBoard[y-1][x].lightable = False
-    elif (x >= len(processedPuzzleBoard[y])): #If the 0 is on the right of the board
+    if (x >= len(processedPuzzleBoard[y])): #If the 0 is on the right of the board
         processedPuzzleBoard[y][x-1].lightable = False
         processedPuzzleBoard[y+1][x].lightable = False
         processedPuzzleBoard[y-1][x].lightable = False
-    elif (y <= 0): #If the 0 is on the top of the board
+    if (y <= 0): #If the 0 is on the top of the board
         processedPuzzleBoard[y][x+1].lightable = False
         processedPuzzleBoard[y][x-1].lightable = False
         processedPuzzleBoard[y+1][x].lightable = False
-    elif (y >= len(processedPuzzleBoard)): #If the 0 is on the bottom of the board
+    if (y >= len(processedPuzzleBoard)): #If the 0 is on the bottom of the board
         processedPuzzleBoard[y][x+1].lightable = False
         processedPuzzleBoard[y][x-1].lightable = False
         processedPuzzleBoard[y-1][x].lightable = False
@@ -40,3 +51,78 @@ def setZero(puzzleBoard, x , y): #Sets surrounding cells as unlightable
         processedPuzzleBoard[y+1][x].lightable = False
         processedPuzzleBoard[y-1][x].lightable = False
     return processedPuzzleBoard
+
+def setFour(puzzleBoard, x, y):
+    processedPuzzleBoard = puzzleBoard
+    
+    #Places a lightbulb at each adjacent cell then calls the placeBulb function to light all relevant cells
+    try:
+        processedPuzzleBoard[y][x+1].cellType = "L"
+        processedPuzzleBoard = placeBulb(processedPuzzleBoard, x+1, y, "right")
+
+        processedPuzzleBoard[y][x-1].cellType = "L"
+        processedPuzzleBoard = placeBulb(processedPuzzleBoard, x-1, y, "left")
+
+        processedPuzzleBoard[y+1][x].cellType = "L"
+        processedPuzzleBoard = placeBulb(processedPuzzleBoard, x, y+1, "down")
+        
+        processedPuzzleBoard[y-1][x].cellType = "L"
+        processedPuzzleBoard = placeBulb(processedPuzzleBoard, x, y-1, "left")
+
+    except IndexError:
+        print(str(x) + " " + str(y))
+        print("Tried to place a lightbulb outside the gameboard like an idiot")
+    
+    return processedPuzzleBoard
+
+def placeBulb(puzzleBoard, x, y, direction):
+    stopped = False
+    print("Place Bulb called")
+    while not stopped:
+        print("Started Looping")
+        if direction == "right": #Keeps going right until hits either the edge of the board or another type of cell (e.g another number or blocked cell)
+            x += 1
+            if x <= len(puzzleBoard[y]):
+                print("Not out of bounds")
+                if puzzleBoard[y][x].cellType == "-":
+                    print("Changing cell (" + str(x) + "," + str(y) + ") to lit")
+                    puzzleBoard[y][x].lit = True
+                else:
+                    stopped = True
+            else:
+                stopped = True
+        elif direction == "left": #Keeps going left until hits either the edge of the board or another type of cell (e.g another number or blocked cell)
+            x -= 1
+            if x >= 0:
+                if puzzleBoard[y][x].cellType == "-":
+                    print("Changing cell (" + str(x) + "," + str(y) + ") to lit")
+                    puzzleBoard[y][x].lit = True
+                else:
+                    stopped = True
+            else:
+                stopped = True
+        elif direction == "up": #Keeps going up until hits either the edge of the board or another type of cell (e.g another number or blocked cell)
+            y -= 1
+            if y >= 0:
+                if puzzleBoard[y][x].cellType == "-":
+                    print("Changing cell (" + str(x) + "," + str(y) + ") to lit")
+                    puzzleBoard[y][x].lit = True
+                else:
+                    stopped = True
+            else:
+                stopped = True
+        elif direction == "down": #Keeps going down until hits either the edge of the board or another type of cell (e.g another number or blocked cell)
+            y += 1
+            if y <= len(puzzleBoard):
+                if puzzleBoard[y][x].cellType == "-":
+                    print("Changing cell (" + str(x) + "," + str(y) + ") to lit")
+                    puzzleBoard[y][x].lit = True
+                else:
+                    stopped = True
+            else:
+                stopped = True
+        else:
+            print("Something has gone horribly wrong in the set light function")
+        return puzzleBoard
+
+
