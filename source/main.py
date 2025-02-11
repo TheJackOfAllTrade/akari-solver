8e34bcbb7b0614e6ab5be36472963dedf21a9307
TheJackOfAllTrade <153755269+TheJackOfAllTrade@users.noreply.github.com>
2025-02-11 12:44:49 +0000

    placeBulb() places Bulbs
    Place bulb places bulbs now
    
    made a way to output to csv
    
    begun 3s preprocessing
    
    formatted some of the puzzles so they can be read by the script (no data was deleted)

--- source/main.py
+++ source/main.py
@@ -8,48 +8,54 @@ class Cell: #Each cell in the puzzle board and its attributes
         self.lightable = lightable #Don't be fooled, "Lightable" in this case means a cell can't have a lightbulb. I'm just dumb and too lazy to change it
 
     def placeBulb(self, board, x, y):
-
         directions = ["up","down","left","right"]
-        for i in directions:
-            stopped = False
-            yCoord = y
-            xCoord = x
-            while not stopped:
-                if board[yCoord][xCoord].cellType == "-" or board[yCoord][xCoord].cellType == "L": 
-                    if i == "up":
-                        board[yCoord][xCoord].lit = True
-                        if yCoord <= 0:
-                            stopped = True
-                        else:
-                            yCoord -= 1
-                    elif i == "down":
-                        print(str(yCoord))
-                        board[yCoord][xCoord].lit = True
-                        if yCoord >= (len(board) - 1):
-                            stopped = True
-                        else:
-                            yCoord += 1
-                    elif i == "left":
-                        board[yCoord][xCoord].lit = True
-                        if xCoord <= 0:
-                            stopped = True
-                        else:
-                            xCoord -= 1
-                    elif i == "right":
-                        board[yCoord][xCoord].lit = True
-                        if xCoord >= (len(board[0]) - 1):
-                            stopped = True
-                        else:    
-                            xCoord += 1
-                    else:
-                        print("OOOOOOO SCHOOOBIDY LOOBIDY!!")
-                else:
-                    print("Met Cell type [" + board[yCoord][xCoord].cellType + "]. Moving on...")
-                    stopped = True
-        return board
+        for l in directions: #I actually can't figure out the logic of why this works as I used it for a complete different reason. If it is removed it will no longer work properly.
+            if board[y][x].lightable == True:
+                board[y][x].cellType = "L"
+            else:
+                print("A lightbulb actually wasn't placed")
 
-def readFile(): #Reads the file and formats each cell into the Cell object, adding them to the puzzleBoard array
-    file = open("puzzles/7x7-6", "r")
+        
+            for i in directions:
+                stopped = False
+                yCoord = y
+                xCoord = x
+                while not stopped:
+                    if board[yCoord][xCoord].cellType == "-" or board[yCoord][xCoord].cellType == "L": 
+                        if i == "up":
+                            board[yCoord][xCoord].lit = 1
+                            if yCoord <= 0:
+                                stopped = True
+                            else:
+                                yCoord -= 1
+                        elif i == "down":
+                            print(str(yCoord))
+                            board[yCoord][xCoord].lit = 1
+                            if yCoord >= (len(board) - 1):
+                                stopped = True
+                            else:
+                                yCoord += 1
+                        elif i == "left":
+                            board[yCoord][xCoord].lit = 1
+                            if xCoord <= 0:
+                                stopped = True
+                            else:
+                                xCoord -= 1
+                        elif i == "right":
+                            board[yCoord][xCoord].lit = 1
+                            if xCoord >= (len(board[0]) - 1):
+                                stopped = True
+                            else:    
+                                xCoord += 1
+                        else:
+                            print("OOOOOOO SCHOOOBIDY LOOBIDY!!")
+                    else:
+                        print("Met Cell type [" + board[yCoord][xCoord].cellType + "]. Moving on...")
+                        stopped = True
+            return board
+
+def readFile(filename): #Reads the file and formats each cell into the Cell object, adding them to the puzzleBoard array
+    file = open(filename, "r")
     content = file.readlines()
 
     for i in content[7:(len(content)-1)]:
@@ -58,15 +64,17 @@ def readFile(): #Reads the file and formats each cell into the Cell object, addi
             if x == " " or repr(x) == repr("\n"):
                 next
             elif x == "-":
-                newCell = Cell(x, False, True)
+                newCell = Cell(x, 0, 1)
                 currentLine.append(newCell)
             else:
-                newCell = Cell(x, False, False)
+                newCell = Cell(x, 0, 0)
                 currentLine.append(newCell)
         puzzleBoard.append(currentLine)
     file.close()
 
 def outputBoard(puzzleBoard, type):
+
+    # Output Types: (cell, lit, lightable)
     if type == "cell":
         for y in range(len(puzzleBoard)):
             line = []
@@ -88,21 +96,52 @@ def outputBoard(puzzleBoard, type):
     else:
         print("Not a valid output type")
 
+def writeBoardStateToCSV(puzzleBoard, type):
+    # Output Types: (cell, lit, lightable)
+    name = filename[8:len(filename)+1] + type + ".csv" 
+    filepath = "csv/" + name
+    print(filepath)
+    csv = open(filepath, "w")
+    if type == "cell":
+        for y in range(len(puzzleBoard)):
+            for x in range(len(puzzleBoard[0])):
+                csv.write(puzzleBoard[y][x].cellType + ",")
+            csv.write("\n")
+    elif type == "lit":
+        for y in range(len(puzzleBoard)):
+            for x in range(len(puzzleBoard[0])):
+                csv.write(str(puzzleBoard[y][x].lit) + ",")
+            csv.write("\n")
+    elif type == "lightable":
+        for y in range(len(puzzleBoard)):
+            for x in range(len(puzzleBoard[0])):
+                csv.write(str(puzzleBoard[y][x].lightable) + ",")
+            csv.write("\n")
+    else:
+        print("Not a valid output type (csv method)")
+
+        
+
 puzzleBoard = [] #The initial puzzleboard, a 2D array of "Cell" Objects
-readFile() #Initialises puzzleBoard
+filename = "puzzles/20x20-1"
+readFile(filename) #Initialises puzzleBoard
 print("Puzzle Initialised!")
 
 proPuzzleBoard = pp.preProcess(puzzleBoard) #Calls function in preprocessing.py
 print("Puzzle preprocessed")
 
-
+print("#####Cell#####")
 outputBoard(proPuzzleBoard, "cell")
+print("#####LIT#####")
 outputBoard(proPuzzleBoard, "lit")
+print("#####LIGHTABLE#####")
+outputBoard(proPuzzleBoard, "lightable")
 
-#for i in range(len(proPuzzleBoard)):
-#    print("##########################")
-#    print(proPuzzleBoard[i][5].cellType)
-#    print(proPuzzleBoard[i][5].lit)
+
+writeBoardStateToCSV(proPuzzleBoard, "cell")
+writeBoardStateToCSV(proPuzzleBoard, "lit")
+writeBoardStateToCSV(proPuzzleBoard, "lightable")
+
 
 
 
--- source/preprocessing.py
+++ source/preprocessing.py
@@ -5,6 +5,7 @@ def preProcess(puzzleBoard): #What is called in the main
     processedPuzzleBoard = puzzleBoard #Doesn't overwrite puzzleBoard just in case
     processedPuzzleBoard = lookForZero(processedPuzzleBoard)
     processedPuzzleBoard = lookForFour(processedPuzzleBoard)
+    processedPuzzleBoard = lookForRest(processedPuzzleBoard)
     return processedPuzzleBoard
 
 def lookForZero(processedPuzzleBoard): #Searches the board for any cells containing a zero
@@ -27,30 +28,28 @@ def lookForFour(processedPuzzleBoard): #Searches the board for any cells contain
                 next
     return processedPuzzleBoard
 
+def lookForRest(processedPuzzleBoard):
+    for y in range(len(processedPuzzleBoard)):
+        for x in range(len(processedPuzzleBoard[y])):
+            if processedPuzzleBoard[y][x].cellType == "3" or processedPuzzleBoard[y][x].cellType == "2" or processedPuzzleBoard[y][x].cellType == "1":
+                print("Found " + processedPuzzleBoard[y][x].cellType + " at (" + str(x) + "," + str(y) + ")")
+                processedPuzzleBoard = dealWithRest(processedPuzzleBoard, x, y)
+            else:
+                next
+    return processedPuzzleBoard
+
+
 def setZero(puzzleBoard, x , y): #Sets surrounding cells as unlightable
     processedPuzzleBoard = puzzleBoard
-    if (x <= 0): #If the 0 is on the left of the board
-        processedPuzzleBoard[y][x+1].lightable = False
-        processedPuzzleBoard[y+1][x].lightable = False
-        processedPuzzleBoard[y-1][x].lightable = False
-    if (x >= len(processedPuzzleBoard[y])): #If the 0 is on the right of the board
-        processedPuzzleBoard[y][x-1].lightable = False
-        processedPuzzleBoard[y+1][x].lightable = False
-        processedPuzzleBoard[y-1][x].lightable = False
-    if (y <= 0): #If the 0 is on the top of the board
-        processedPuzzleBoard[y][x+1].lightable = False
-        processedPuzzleBoard[y][x-1].lightable = False
-        processedPuzzleBoard[y+1][x].lightable = False
-    if (y >= len(processedPuzzleBoard)): #If the 0 is on the bottom of the board
-        processedPuzzleBoard[y][x+1].lightable = False
-        processedPuzzleBoard[y][x-1].lightable = False
-        processedPuzzleBoard[y-1][x].lightable = False
-    else: #Set all adjacent tiles to unlightable
-        processedPuzzleBoard[y][x+1].lightable = False
-        processedPuzzleBoard[y][x-1].lightable = False
-        processedPuzzleBoard[y+1][x].lightable = False
-        processedPuzzleBoard[y-1][x].lightable = False
-    return processedPuzzleBoard
+    try:
+        processedPuzzleBoard[y][x+1].lightable = 0
+        processedPuzzleBoard[y][x-1].lightable = 0
+        processedPuzzleBoard[y+1][x].lightable = 0
+        processedPuzzleBoard[y-1][x].lightable = 0
+    except IndexError:
+        print()
+    finally:
+        return processedPuzzleBoard
 
 def setFour(puzzleBoard, x, y):
     processedPuzzleBoard = puzzleBoard
@@ -60,26 +59,27 @@ def setFour(puzzleBoard, x, y):
         catchx = x+1
         catchy = y
         catchdirection = "right"
-        processedPuzzleBoard[y][x+1].cellType = "L"
+        #processedPuzzleBoard[y][x+1].cellType = "L"
         processedPuzzleBoard = processedPuzzleBoard[y][x+1].placeBulb(processedPuzzleBoard, x+1, y)
 
         catchx = x-1
         catchy = y
         catchdirection = "left"
-        processedPuzzleBoard[y][x-1].cellType = "L"
+        #processedPuzzleBoard[y][x-1].cellType = "L"
         processedPuzzleBoard = processedPuzzleBoard[y][x-1].placeBulb(processedPuzzleBoard, x-1, y)
 
         catchx = x
         catchy = y+1
         catchdirection = "down"
-        processedPuzzleBoard[y+1][x].cellType = "L"
+        #processedPuzzleBoard[y+1][x].cellType = "L"
         processedPuzzleBoard = processedPuzzleBoard[y+1][x].placeBulb(processedPuzzleBoard, x, y+1)
         
         catchx = x
         catchy = y-1
         catchdirection = "up"
-        processedPuzzleBoard[y-1][x].cellType = "L"
+        #processedPuzzleBoard[y-1][x].cellType = "L"
         processedPuzzleBoard = processedPuzzleBoard[y-1][x].placeBulb(processedPuzzleBoard, x, y-1)
+
     
     except IndexError:
         print("Tried to place a lightbulb outside the gameboard like an idiot at " + str(catchx) + " " + str(catchy) + " in the " + catchdirection + " direction")
@@ -87,4 +87,48 @@ def setFour(puzzleBoard, x, y):
     finally:
         return processedPuzzleBoard
 
+def dealWithRest(puzzleBoard, x, y):
+    if(puzzleBoard[y][x].cellType == "3"):
+        surroundings = countSurroundings(puzzleBoard, x, y)
+        print("There are " + str(surroundings) + " valid cells around (" + str(x) + "," + str(y) + ")")
+    elif(puzzleBoard[y][x].cellType == "2"):
+        print("Hmm...Yes, this is a 2")
+    elif(puzzleBoard[y][x].cellType == "1"):
+        print("Hmm...Yes, this is a 1")
+    else:
+        print("Hmm...I have no clue what this is.")
+    
+    return puzzleBoard
 
+def countSurroundings(puzzleBoard, x, y):
+    count = 0
+    directions = ["up", "down", "left", "right"]
+    for i in directions:
+        try:
+            if i == "up":
+                cell = puzzleBoard[y-1][x]
+                if cell.cellType == "-" and cell.lit == 0 and cell.lightable == 1:
+                    count += 1
+                else:
+                    next
+            elif i == "down":
+                cell = puzzleBoard[y+1][x]
+                if cell.cellType == "-" and cell.lit == 0 and cell.lightable == 1:
+                    count += 1
+                else:
+                    next
+            elif i == "left":
+                cell = puzzleBoard[y][x-1]
+                if cell.cellType == "-" and cell.lit == 0 and cell.lightable == 1:
+                    count += 1
+                else:
+                    next
+            elif i == "right":
+                cell = puzzleBoard[y][x+1]
+                if cell.cellType == "-" and cell.lit == 0 and cell.lightable == 1:
+                    count += 1
+                else:
+                    continue
+        except IndexError:
+                next
+    return count
